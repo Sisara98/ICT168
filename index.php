fe1affaa8154b28e92853d3f1aee7a65a36745ca
Sisara98 <sisaradilhari.1998@gmail.com>
2022-01-11 16:59:22 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+class StudentParent{
+            public $stdName;
+            public $stdRegNum;
+            
+            public function __construct($stdName,$stdRegNum){
+                $this->stdName = $stdName;
+                $this->stdRegNum = $stdRegNum;
+            }
+            
+        }
+        ?>
